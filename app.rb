d664284accc86dfde9e5401773c9e3325ba421a0
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-01-22 16:32:38 +0000

    configured a helper class to perform signing in and entering game

--- app.rb
+++ app.rb
@@ -1,10 +1,24 @@
 require 'sinatra'
 require 'shotgun'
 
-class Battle < Sinatra::Base
 
+class Battle < Sinatra::Base
+  enable :sessions
+  set :session_secret, "SuparSekrit"
 get '/' do
   erb(:index)
+end
+
+post '/names' do
+  session[:player_1_name] = params[:player_1_name]
+  session[:player_2_name] = params[:player_2_name]
+  redirect '/play'
+end
+
+get '/play' do
+  @player_1_name = session[:player_1_name]
+  @player_2_name = session[:player_2_name]
+  erb(:play)
 end
 
 end

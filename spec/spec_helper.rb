99d3d9b47c0feb91384856f84198c52db4b53c6f
Kai Hoffman <44998809+kaihoffman@users.noreply.github.com>
2019-01-22 15:04:07 +0000

    first feature test complete

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,14 @@
 # it.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+ENV["RACK_ENV"] = 'test'
+require File.join(File.dirname(__FILE__), '..', 'app.rb')
+require 'capybara'
+require 'capybara/rspec'
+require 'rspec'
+
+Capybara.app = Battle
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
@@ -97,4 +105,7 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+config.default_formatter = "doc"
+
+
 end
